78e091115fa5af1a3607eaa5d8e32a8eec4c7a4a
Maciej Kucharski <maciek.zir@gmail.com>
2016-07-01 08:18:42 +0200

    [master][feature] app menu

---
+++ app/desktop/application_menu/app.js
@@ -0,0 +1,3 @@
+export const ApplicationMenu = {
+  label: 'Pomodoro'
+};
--- app/desktop/application_menu/index.js
+++ app/desktop/application_menu/index.js
@@ -1,8 +1,11 @@
 import { Menu } from 'electron';
 
 import { DevelopmentMenu } from './development';
+import { ApplicationMenu } from './app';
 
-const template = [];
+const template = [
+  ApplicationMenu
+];
 
 if (process.env.NODE_ENV === 'development') {
   template.push(DevelopmentMenu);
